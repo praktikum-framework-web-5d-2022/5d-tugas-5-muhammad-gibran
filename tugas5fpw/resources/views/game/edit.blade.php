322add0e7ecfde0c80e3431467413cdf60f09b7c
Wiz Grimoire <2010631170097@student.unsika.ac.id>
2022-12-02 23:43:32 +0700

    Tugas5

---
+++ tugas5fpw/resources/views/game/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('master')
+@section('title','Data Game')
+
+@section('content')
+<style>
+    .bg-blue {
+        background-color: blue;
+        color: white;
+    }
+</style>
+<div class="container pt-4 bg-white">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Edit Data Game</h2>
+            <p>Masukkan data game dengan lengkap</p>
+            @if (session()->has('message'))
+            <div class="my-3">
+                <div class="alert alert-success">
+                    {{session()->get('message')}}
+                </div>
+            </div>
+            @endif
+            <form action="{{route('game.update', ['game' => $game->id])}}" method="POST">
+                @method('PATCH')
+                @csrf
+                <div class="mb-4">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" name="nama" id="nama" placeholder="Masukkan Nama" class="form-control" value="{{old('nama')?? $game->nama}}">
+                    @error('nama')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="dev" class="form-label">Developer</label>
+                    <input type="text" name="dev" id="dev" placeholder="Masukkan Nama Developer" class="form-control" value="{{old('dev')?? $game->dev}}">
+                    @error('dev')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="genre" class="form-label">Genre</label>
+                    <select name="genre" id="genre" class="form-control">
+                        <option value="fantasy" {{old('genre') == "fantasy" ? "selected" : ""}}>fantasy</option>
+                        <option value="action" {{old('genre') == "action" ? "selected" : ""}}>action</option>
+                        <option value="racing" {{old('genre') == "racing" ? "selected" : ""}}>racing</option>
+                        <option value="rhythm" {{old('genre') == "rhythm" ? "selected" : ""}}>rhythm</option>
+                        <option value="shooter" {{old('genre') == "shooter" ? "selected" : ""}}>shooter</option>
+                    </select>
+                    @error('genre')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="publisher" class="form-label">Publisher</label>
+                    <input type="text" name="publisher" id="publisher" placeholder="Masukkan Nama Publisher" class="form-control" value="{{old('publisher')?? $game->datagame->publisher}}">
+                    @error('publisher')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="platform" class="form-label">Platform</label>
+                    <select name="platform" id="platform" class="form-control">
+                        <option value="mobile" {{old('platform') == "mobile" ? "selected" : ""}}>mobile</option>
+                        <option value="console" {{old('platform') == "console" ? "selected" : ""}}>console</option>
+                        <option value="pc" {{old('platform') == "pc" ? "selected" : ""}}>pc</option>
+                    </select>
+                    @error('platform')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="rating" class="form-label">Rating</label>
+                    <input type="text" name="rating" id="rating" placeholder="Masukkan Rating dari game" class="form-control" value="{{old('rating')?? $game->datagame->rating}}">
+                    @error('rating')
+                    <div class="text-danger">{{$message}}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn bg-info"><b class="text-white">Simpan</b></button>
+                <p></p>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
